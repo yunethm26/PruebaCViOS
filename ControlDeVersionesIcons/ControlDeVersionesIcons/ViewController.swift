38b5cf6d3ef3b9afa0fe56a782a30f1381749ff1
Diana <diana.arnao@telefonica.com>
2019-08-27 14:12:07 -0400

    Prueba control de versiones

---
+++ ControlDeVersionesIcons/ControlDeVersionesIcons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ControlDeVersionesIcons
+//
+//  Created by Diana Arnao on 8/27/19.
+//  Copyright © 2019 telefonica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
